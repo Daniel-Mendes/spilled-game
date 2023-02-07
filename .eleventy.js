071f8151f174922fdd5b313833a3685dfd571050
Daniel Mendes <contact@daniel-mendes.ch>
2023-02-07 21:31:53 +0100

    fix: Fixed code not bundling

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-var markdownItEleventyImg = require('markdown-it-eleventy-img');
+const markdownItEleventyImg = require('markdown-it-eleventy-img');
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
